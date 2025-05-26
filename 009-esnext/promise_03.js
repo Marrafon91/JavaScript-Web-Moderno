d661bf5ea58af38b3dd8d46218d7b4660f75c6aa
GuiM <Guilherme.Marrafon1@gmail.com>
2025-05-26 17:09:08 -0300

    Summary

---
+++ 009-esnext/promise_03.js
@@ -0,0 +1,16 @@
+function rand(min, max) {
+  if (min > max) {
+    [max, min] = [min, max];
+  }
+
+  return new Promise((resolve) => {
+    const fator = max - min + 1;
+    const aleatorio = parseInt(Math.random() * fator) + min;
+    resolve(aleatorio);
+  });
+}
+
+rand(1, 60)
+  .then((num) => num * 10)
+  .then((numX10) => `O numero gerado foi ${numX10}`)
+  .then(console.log);
