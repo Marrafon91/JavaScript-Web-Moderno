b55988ea5d90792068a70842a0dd67a415dd53d5
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-05 13:23:49 -0300

    Summary

--- 010-ExerciciosWeb/form/server.js
+++ 010-ExerciciosWeb/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true}))
 
 app.post('/usuarios', (req, res) => {
     console.log(req.body)
-    res.send('<h1>Parabens</h1>')
+    res.send('<h1>Parabéns. Usuário incluido!</h1>')
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send('<h1>Parabéns. Usuário Alterado!</h1>')
 })
 
 app.listen(3003)
